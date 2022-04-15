ca0475901b169c2fdc66646a9ffea51af34ca1f8
Cole Callaway <nccallaway54@gmail.com>
2022-04-15 12:40:51 -0600

    delete done

--- delete.sql
+++ delete.sql
@@ -1,10 +1,31 @@
--- INSERT INTO animals ( name, type, age ) 
--- VALUES ('Leo', 'lion', 12),
--- ('Jerry', 'mouse', 4),
--- ('Marty', 'zebra', 10),
--- ('Gloria', 'hippo', 8),
--- ('Alex', 'lion', 9),
--- ('Melman', 'giraffe', 15),
--- ('Nala', 'lion', 2),
--- ('Marie', 'cat', 1),
--- ('Flounder', 'fish', 8);
+create table animals ( 
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(50), 
+    type TEXT, 
+    age INTEGER 
+);
+
+INSERT INTO animals ( name, type, age ) 
+VALUES ('Leo', 'lion', 12),
+('Jerry', 'mouse', 4),
+('Marty', 'zebra', 10),
+('Gloria', 'hippo', 8),
+('Alex', 'lion', 9),
+('Melman', 'giraffe', 15),
+('Nala', 'lion', 2),
+('Marie', 'cat', 1),
+('Flounder', 'fish', 8);
+
+select * from animals
+
+DELETE
+from animals
+WHERE type = 'lion';
+
+DELETE
+from animals
+WHERE name like 'M%';
+
+DELETE
+from animals
+WHERE age < 9;
